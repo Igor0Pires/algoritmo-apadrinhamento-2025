5cbbcf6a6b7ab5bdd1392624d7e09f0eee3137b6
Igor0Pires <igorpife@usp.br>
2025-04-23 20:29:44 -0300

    feat: Generalizei o código para funcionar em qualquer csv

--- app/app.py
+++ app/app.py
@@ -23,51 +23,76 @@ st.header("Upload de Arquivos")
 uploaded_file_afilhados = st.file_uploader("Faça upload do arquivo CSV de afilhados", type=["csv"])
 uploaded_file_padrinhos = st.file_uploader("Faça upload do arquivo CSV de padrinhos", type=["csv"])
 
+if 'x' not in st.session_state:
+        st.session_state.x = 0
+x = st.session_state.x
 # Carregar dados
 if uploaded_file_afilhados is not None and uploaded_file_padrinhos is not None:
-    # Lê os arquivos CSV
-    df_afilhados = pd.read_csv(uploaded_file_afilhados)
-    df_padrinhos = pd.read_csv(uploaded_file_padrinhos)
+    try:
+        # Lê os arquivos CSV
+        df_afilhados = pd.read_csv(uploaded_file_afilhados)
+        df_padrinhos = pd.read_csv(uploaded_file_padrinhos)
 
-    # Filtra as colunas relevantes
-    df_afilhados = filtro_colunas(df_afilhados, [2, 10, 11, 12, 13, 14, 15])
-    df_padrinhos = filtro_colunas(df_padrinhos, [2, 18, 19, 20, 21, 22, 23, 24])
+        # Multi seleção para escolher as colunas relevantes
+        st.subheader("Selecione as colunas relevantes para os afilhados")
+        colunas_afilhados = df_afilhados.columns.tolist()
+        colunas_afilhados_selecionadas = st.multiselect("Colunas disponíveis:", colunas_afilhados, default=colunas_afilhados)
 
-    # Transforma todas as células em string
-    df_afilhados = df_afilhados.map(str)
-    df_padrinhos.iloc[:, :-1] = df_padrinhos.iloc[:, :-1].map(str)
+        st.subheader("Selecione as colunas relevantes para os padrinhos")
+        colunas_padrinhos = df_padrinhos.columns.tolist()
+        colunas_padrinhos_selecionadas = st.multiselect("Colunas disponíveis:", colunas_padrinhos, default=colunas_padrinhos)
 
-        # Renomeia a primeira coluna para 'Nome completo'
-    df_afilhados.rename(columns={df_afilhados.columns[0]: 'Nome completo'}, inplace=True)
-    df_padrinhos.rename(columns={df_padrinhos.columns[0]: 'Nome completo'}, inplace=True)
+        # Lista as colunas selecionadas como indices
+        indices_afilhados = [colunas_afilhados.index(col) for col in colunas_afilhados_selecionadas]
+        indices_padrinhos = [colunas_padrinhos.index(col) for col in colunas_padrinhos_selecionadas]
+
+        # Filtra as colunas relevantes
+        df_afilhados = filtro_colunas(df_afilhados, indices_afilhados)
+        df_padrinhos = filtro_colunas(df_padrinhos, indices_padrinhos)
+        
+
+        # Transforma todas as células em string
+        df_afilhados = df_afilhados.map(str)
+        df_padrinhos.iloc[:, :-1] = df_padrinhos.iloc[:, :-1].map(str)
+
+            # Renomeia a primeira coluna para 'Nome completo'
+        df_afilhados.rename(columns={df_afilhados.columns[0]: 'Nome completo'}, inplace=True)
+        df_padrinhos.rename(columns={df_padrinhos.columns[0]: 'Nome completo'}, inplace=True)
+        
+        # Renomeia a última coluna para 'Numero de afilhados'
+        df_padrinhos.rename(columns={df_padrinhos.columns[-1]: 'Numero de afilhados'}, inplace=True)
+
+        # Armazena os DataFrames na sessão
+        st.session_state.df_afilhados = df_afilhados
+        st.session_state.df_padrinhos = df_padrinhos
+    except Exception as e:
+        pass
+
+    # Alterar número de afilhados por padrinho
+    x += int(st.button("Filtrar Colunas"))
+if x and 'df_padrinhos' in st.session_state:
     
-    # Renomeia a última coluna para 'Numero de afilhados'
-    df_padrinhos.rename(columns={df_padrinhos.columns[-1]: 'Numero de afilhados'}, inplace=True)
-
-    # Armazena os DataFrames na sessão
-    st.session_state.df_afilhados = df_afilhados
-    st.session_state.df_padrinhos = df_padrinhos
-
-# Alterar número de afilhados por padrinho
-if 'df_padrinhos' in st.session_state:
-    st.header("Alterar Número de Afilhados Aceitos pelo Padrinho")
-    padrinho_selecionado = st.selectbox(
-        "Selecione um padrinho:",
-        st.session_state.df_padrinhos['Nome completo']
-    )
-    novo_limite = st.number_input(
-        f"Novo número de afilhados aceitos para {padrinho_selecionado}:",
-        min_value=0,
-        value=int(st.session_state.df_padrinhos.loc[
-            st.session_state.df_padrinhos['Nome completo'] == padrinho_selecionado, 'Numero de afilhados'
-        ].values[0])
-    )
-    if st.button("Atualizar Limite"):
-        st.session_state.df_padrinhos.loc[
-            st.session_state.df_padrinhos['Nome completo'] == padrinho_selecionado, 'Numero de afilhados'
-        ] = novo_limite
-        st.success(f"Limite de afilhados para {padrinho_selecionado} atualizado para {novo_limite}!")
-
+    try:
+        st.header("Alterar Número de Afilhados Aceitos pelo Padrinho")
+        padrinho_selecionado = st.selectbox(
+            "Selecione um padrinho:",
+            st.session_state.df_padrinhos['Nome completo']
+        )
+        
+        novo_limite = st.number_input(
+            f"Novo número de afilhados aceitos para {padrinho_selecionado}:",
+            min_value=0,
+            value=int(st.session_state.df_padrinhos.loc[
+                st.session_state.df_padrinhos['Nome completo'] == padrinho_selecionado, 'Numero de afilhados'
+            ].values[0])
+        )
+        if st.button("Atualizar Limite"):
+            st.session_state.df_padrinhos.loc[
+                st.session_state.df_padrinhos['Nome completo'] == padrinho_selecionado, 'Numero de afilhados'
+            ] = novo_limite
+            st.success(f"Limite de afilhados para {padrinho_selecionado} atualizado para {novo_limite}!")
+    except Exception as e:
+        st.error(f"Erro ao filtrar colunas. Por favor, verifique se filtros foram aplicados corretamente. Erro: {e}")
 # Fazer match batch
 if 'df_afilhados' in st.session_state and 'df_padrinhos' in st.session_state:
     st.header("Realizar Match (Batch)")
